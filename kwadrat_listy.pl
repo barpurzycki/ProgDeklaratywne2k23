3f70226466c7a9e224419eb511ce380bf0dd8892
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2023-03-27 12:13:23 +0200

    Add files via upload

---
+++ kwadrat_listy.pl
@@ -0,0 +1,16 @@
+% kwadrat_listy(+L1,?L2).
+% spełniony, gdy elementy listy L2 są kwadratami
+% elementów listy l1; lista L1 jest listą liczbową
+% ograniczenia: L1 i L2 są listami liczbowymi
+% ------------------------------------------------
+
+% warunek kończący rekurencję: kwadrat listy pustej
+% jest listą pustą.
+
+kwadrat_listy([],[]).
+
+% rekurencja:
+
+kwadrat_listy([H1|T1], [H2|T2]):-
+		H2 is H1*H1,
+		kwadrat_listy(T1,T2).
