01d1e7e074bd1ffe41c229b79b55da6806285b5f
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2023-04-03 12:43:24 +0200

    Add files via upload

---
+++ predykaty.pl
@@ -0,0 +1,29 @@
+% Predykaty
+
+% dodatni(X)
+% spełniony, gdy X liczbą dodatnią
+
+		dodatni(X):-X>0.
+		
+		
+% lista_dodatnia(L)
+% spełniony, gdy wszystkie elementy listy L są dodatnie
+
+		lista_dodatnia(L):-
+				maplist(dodatni,L).
+				
+				
+% wiekszy_od(X,Y)
+% spełniony, gdy Y jest większe od X
+
+		większy_od(X,Y):-Y>X.
+		
+% wiekszy_od_lista(+X,+L).
+% spełniony, gdy wszystkie elementy listy L
+% są większe od X
+
+		wiekszy_od_lista(X,L):-
+				maplist(wiekszy_od(X),L).
+				
+		kwadrat(X,Y):-Y is X*X.
+		kwadrat_listy(L1,L2):-maplist(kwadrat,L1,L2).
